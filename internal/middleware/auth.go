f61525a885ceb9d083dad87cadd5e7ed58fef027
Dmitriy Trofimov <a.dev.mobile@gmail.com>
2024-01-28 08:56:42 +0300

    refactor: added AuthMiddleware statuses

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -15,32 +15,52 @@ type ContextKey string
 // Определение константы для ключа userID
 const UserIDKey ContextKey = "userID"
 
+// AuthErrorResponse структура для ответов об ошибках аутентификации
+type AuthErrorResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 // AuthMiddleware создает middleware для проверки JWT токена.
 func AuthMiddleware(jwtSecret string) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		// Извлечение токена из заголовка Authorization
 		authHeader := c.GetHeader("Authorization")
 		if authHeader == "" {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Authorization header is required"})
+			c.AbortWithStatusJSON(http.StatusUnauthorized, AuthErrorResponse{Status: "AUTHORIZATION_REQUIRED", Message: "Authorization header is required"})
 			return
 		}
 
 		// Проверка формата токена (обычно это 'Bearer [token]')
 		token := strings.TrimPrefix(authHeader, "Bearer ")
 		if token == authHeader {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Invalid token format"})
+			c.AbortWithStatusJSON(http.StatusUnauthorized, AuthErrorResponse{Status: "INVALID_TOKEN_FORMAT", Message: "Invalid token format. Format should be 'Bearer [token]'."})
 			return
 		}
 
 		// Парсинг и валидация токена
 		userID, err := utils.ParseToken(token, jwtSecret, "access")
 		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": err.Error()})
+			var status string
+			var message string
+			switch err.Error() {
+			case "Token expired":
+				status = "TOKEN_EXPIRED"
+				message = "Your token has expired. Please log in again."
+			case "Invalid token":
+				status = "INVALID_TOKEN"
+				message = "The provided token is invalid. Check the token and try again."
+			default:
+				status = "AUTHENTICATION_FAILED"
+				message = "Error occurred during token validation. Please try again."
+			}
+
+			c.AbortWithStatusJSON(http.StatusUnauthorized, AuthErrorResponse{Status: status, Message: message})
 			return
 		}
 
 		// Добавление userID в контекст Gin
-		c.Set("userID", userID)
+		c.Set(string(UserIDKey), userID)
 
 		// Переход к следующему обработчику
 		c.Next()
